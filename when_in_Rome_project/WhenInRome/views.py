b04ee6f32b82f89aeeb44f3bc81c71a84de1348c
Sophie DelMonico <2979687D@student.gla.ac.uk>
2026-03-05 14:35:58 +0000

    Updated views.py and urls.py

--- when_in_Rome_project/WhenInRome/views.py
+++ when_in_Rome_project/WhenInRome/views.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.http import HttpResponse
 
 def index(request):
-    pass
+    return HttpResponse("When In Rome")
--- when_in_Rome_project/when_in_Rome_project/urls.py
+++ when_in_Rome_project/when_in_Rome_project/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from WhenInRome import views
+from django.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
