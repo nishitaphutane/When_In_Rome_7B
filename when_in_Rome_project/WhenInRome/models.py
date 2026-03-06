9361f97a9adac3b2c861c3b2fe9621a8f43ac487
Lewis Fong <3007573@student.gla.ac.uk>
2026-03-06 10:29:47 +0000

    most of admin and models

--- when_in_Rome_project/WhenInRome/models.py
+++ when_in_Rome_project/WhenInRome/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    country = models.CharField(max_length=128, default='Unknown')
+    description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True)
+
+    def save(self, args, **kwargs):
+        self.slug = slugify(self.name)
+        super(City, self).save(args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Cities'
+
+    def __str__(self):
+        return self.name
+
+class Recommendation(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    location = models.CharField(max_length=256, blank=True)
+    slug = models.SlugField(unique=True)
+
+    def save(self, args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Recommendation, self).save(args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+class Review(models.Model):
+    recommendation = models.ForeignKey(Recommendation, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.recommendation.title} - {self.rating}"
