c2a03bbb0871c98db1a8f11f9a51cfe3e58d3755
Lewis Fong <3007573@student.gla.ac.uk>
2026-03-13 10:42:09 +0000

    finsihed models and admin

--- when_in_Rome_project/WhenInRome/admin.py
+++ when_in_Rome_project/WhenInRome/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from WhenInRome.models import City, Recommendation, Review, UserProfile, Upvote
 
-class CityAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}  # Like CategoryAdmin in Rango
+#fills the slug field automatically based on the name of the city or title of the recommendation (all)
 
+class CityAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+#Displays information in list view
 class RecommendationAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
-    list_display = ('title', 'city', 'user')  # Like PageAdmin in Rango
+    list_display = ('title', 'city', 'user') 
 
+#Displays information in list view
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('user', 'recommendation', 'rating')
 
+
+#Registering the models with admin site
 admin.site.register(City, CityAdmin)
 admin.site.register(Recommendation, RecommendationAdmin)
 admin.site.register(Review, ReviewAdmin)
 admin.site.register(UserProfile)
+admin.site.register(Upvote)
--- when_in_Rome_project/WhenInRome/models.py
+++ when_in_Rome_project/WhenInRome/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
+#model to store information about cities
+
 class City(models.Model):
     name = models.CharField(max_length=128, unique=True)
     country = models.CharField(max_length=128, default='Unknown')
     description = models.TextField(blank=True)
     slug = models.SlugField(unique=True)
 
-    def save(self, args, **kwargs):
+    def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(City, self).save(args, **kwargs)
+        super(City, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Cities'
@@ -18,6 +20,8 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+
+#model for recommendations to visit places 
 class Recommendation(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -26,13 +30,15 @@ class Recommendation(models.Model):
     location = models.CharField(max_length=256, blank=True)
     slug = models.SlugField(unique=True)
 
-    def save(self, args, **kwargs):
+    def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
-        super(Recommendation, self).save(args, **kwargs)
+        super(Recommendation, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
 
+
+#model to store user profiles, including bio and profile picture
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
@@ -40,7 +46,8 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
-    
+
+#model to store reviews and ratings for recommendations
 class Review(models.Model):
     recommendation = models.ForeignKey(Recommendation, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -49,3 +56,12 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.recommendation.title} - {self.rating}"
+    
+#model to let users upvote recommendations
+class Upvote(models.Model):
+    recommendation = models.ForeignKey(Recommendation, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} upvoted {self.recommendation.title}"
+    
