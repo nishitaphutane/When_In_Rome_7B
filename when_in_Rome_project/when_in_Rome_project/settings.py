d98b32cf3002b295d3351e2ba1771d6ba7c284fe
Lewis Fong <3007573@student.gla.ac.uk>
2026-03-23 12:29:18 +0000

    fix str in setting

--- when_in_Rome_project/when_in_Rome_project/settings.py
+++ when_in_Rome_project/when_in_Rome_project/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "when_in_Rome_project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
 
