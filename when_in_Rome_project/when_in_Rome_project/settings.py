ac35225c690c38f418b84e1206136ee90dd6bbfd
Eimantas Bogusis <3015017b@student.gla.ac.uk>
2026-03-27 10:52:20 +0000

    Changed one line for deployment

--- when_in_Rome_project/when_in_Rome_project/settings.py
+++ when_in_Rome_project/when_in_Rome_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-(tvk!9@w)@5bpr8)6uw@1z#mo_yulpu7r6@m*qc0wdn%^epeb0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
